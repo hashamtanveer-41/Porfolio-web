924f2ed3359d6ee5a83259f51044aa2081935a05
Hasham Tanveer <hashamtanvr41@gmail.com>
2026-05-29 00:45:30 +0500

    add About section with interactive cards and globe animation

---
+++ src/components/FrameWorks.jsx
@@ -0,0 +1,33 @@
+import {OrbitingCircles} from "./OrbitingCircles.jsx";
+
+export function FrameWorks() {
+    const skills = [
+        "auth0",
+        "cplusplus",
+        "csharp",
+        "css3",
+        "git",
+        "html5",
+        "javascript",
+        "react",
+        "tailwindcss",
+        "vitejs",
+    ];
+    return (
+        <div className="relative flex h-[15rem] w-full flex-col items-center justify-center ">
+            <OrbitingCircles iconSize={40}>
+                {skills.map((skill, index) => (
+                    <Icon key={index} src={`assets/logos/${skill}.svg`} />
+                ))}
+            </OrbitingCircles>
+            <OrbitingCircles iconSize={30} radius={100} reverse speed={2}>
+                {skills.reverse().map((skill, index) => (
+                    <Icon key={index} src={`assets/logos/${skill}.svg`} />
+                ))}
+            </OrbitingCircles>
+        </div>
+    )
+}
+const Icon = ({ src }) => (
+    <img src={src} className="duration-200 rounded-sm hover:scale-110" />
+);
