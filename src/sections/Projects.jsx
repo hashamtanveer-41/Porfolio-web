6b149b0ce1bb03faba27cb65ed66f0c43560aefe
Hasham Tanveer <hashamtanvr41@gmail.com>
2026-05-29 16:06:43 +0500

    add project showcase with details and orbiting circles animation

--- src/sections/Projects.jsx
+++ src/sections/Projects.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import {myProjects} from "../constants/index.js";
+import Project from "../components/Project.jsx";
 
 const Projects = () => {
     return (
-        <div>Projects</div>
+        <section className="relatice c-space section-spacing">
+            <h2 className="text-heading">
+                My Selected Projects
+            </h2>
+            <div className="bg-linear-to-r from-transparent via-neutral-700 to-transparent mt-12  h-[1px] w-full"/>
+            {myProjects.map((project)=> (
+                <Project key={project.id} {...project}/>
+            ))}
+        </section>
     )
 }
 export default Projects;
